bdd8454cb06d2bd892eb4535248ff2a26de696eb
Gus <Gus@None.com>
2025-01-31 23:08:11 -0300

    fix: fix register url

--- gimnasio_front/src/pages/Register.jsx
+++ gimnasio_front/src/pages/Register.jsx
@@ -22,7 +22,7 @@ function Register() {
 
     if (!isLoading) {
         setIsLoading(true);
-        fetch("http://127.0.0.1:5000/register", {
+        fetch("http://127.0.0.1:5000/usuarios/registro", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
